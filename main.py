340a87fc354bf717b4de30db2a168902ffeb97cc
MartinFiorde <fiorde_90@hotmail.com>
2024-10-08 18:10:44 -0300

    game loop and fps limit

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
+# this allows us to use code from the open-source pygame library throughout this file
+# documentation: https://www.pygame.org/docs/ref/pygame.html
 import pygame
 
+from constants import *
+
+
 def main():
-	print("Starting asteroids!")
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
